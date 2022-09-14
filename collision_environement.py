b7b6d42b19fc49f35a05a11e0998ffd31e108fa4
Evan Ollivier <evan.ollivier@akur8.com>
2022-09-14 16:17:30 +0200

    Make the software python3 compatible

--- collision_environement.py
+++ collision_environement.py
@@ -1,103 +1,107 @@
-#coding: utf-8
+# coding: utf-8
 
-#modules externes
+# modules externes
 import sys, os, time, select, tty, termios
 import background
 
-#dans ce dossier nous allons traiter des colisions
-#il y aura l'environement
+# dans ce dossier nous allons traiter des colisions
+# il y aura l'environement
 
-o2=100
-vies=10
-items_to_pass=[' ', '~']
+o2 = 100
+vies = 10
+items_to_pass = [' ', '~']
 
 
 def getO2():
     return o2
 
+
 def setO2(o):
     global o2
-    o2=o
+    o2 = o
+
 
 def oxygene():
-    o2=getO2()
-    y,x=background.getPosition()
-    if y>4:
-        setO2(o2-1)
-        if(o2<0):
-            if(o2%(-5)==0):
-                setVies(getVies()-1)
+    o2 = getO2()
+    y, x = background.getPosition()
+    if y > 4:
+        setO2(o2 - 1)
+        if (o2 < 0):
+            if (o2 % (-5) == 0):
+                setVies(getVies() - 1)
     else:
         setO2(100)
+
 
 def getVies():
     return vies
 
+
 def setVies(o):
     global vies
-    vies=o
+    vies = o
 
 
-
-def moveLeft(y,x, carte):
-    a=background.create(carte)
-    obstacle=False
-    for i in range (y-3, y+2): #hauteur du sous marin
-        if (a[i][x-1] in items_to_pass) and (a[i][x-2] in items_to_pass):
+def moveLeft(y, x, carte):
+    a = background.create(carte)
+    obstacle = False
+    for i in range(y - 3, y + 2):  # hauteur du sous marin
+        if (a[i][x - 1] in items_to_pass) and (a[i][x - 2] in items_to_pass):
             pass
         else:
-            obstacle=True
+            obstacle = True
     return not obstacle
 
-def left(y,x, carte):
-    if moveLeft(y,x, carte):
-        background.setPosition(y,x-2)
+
+def left(y, x, carte):
+    if moveLeft(y, x, carte):
+        background.setPosition(y, x - 2)
 
 
-
-def moveRight(y,x, carte):
-    a=background.create(carte)
-    obstacle=False
-    for i in range (y-3, y+2): #hauteur du sous marin
-        if a[i][x+23] in items_to_pass and (a[i][x+24] in items_to_pass):
+def moveRight(y, x, carte):
+    a = background.create(carte)
+    obstacle = False
+    for i in range(y - 3, y + 2):  # hauteur du sous marin
+        if a[i][x + 23] in items_to_pass and (a[i][x + 24] in items_to_pass):
             pass
         else:
-            obstacle=True
+            obstacle = True
     return not obstacle
 
-def right(y,x, carte):
-    if moveRight(y,x, carte):
-        background.setPosition(y,x+2)
+
+def right(y, x, carte):
+    if moveRight(y, x, carte):
+        background.setPosition(y, x + 2)
 
 
+def moveDown(y, x, carte):
+    a = background.create(carte)
+    obstacle = False
 
-def moveDown(y,x, carte):
-    a=background.create(carte)
-    obstacle=False
-
-    for i in range (x, x+23): #longeur du sous marin
-        if a[y+2][i] in items_to_pass:
+    for i in range(x, x + 23):  # longeur du sous marin
+        if a[y + 2][i] in items_to_pass:
             pass
         else:
-            obstacle=True
+            obstacle = True
     return not obstacle
 
-def down(y,x, carte):
-    if moveDown(y,x, carte):
-        background.setPosition(y+1,x)
+
+def down(y, x, carte):
+    if moveDown(y, x, carte):
+        background.setPosition(y + 1, x)
 
 
-
-def moveUp(y,x, carte):
-    a=background.create(carte)
-    obstacle=False
-    for i in range (x, x+23): #longeur du sous marin
-        if a[y-4][i] in items_to_pass:
+def moveUp(y, x, carte):
+    a = background.create(carte)
+    obstacle = False
+    for i in range(x, x + 23):  # longeur du sous marin
+        if a[y - 4][i] in items_to_pass:
             pass
         else:
-            obstacle=True
+            obstacle = True
     return not obstacle
 
-def up(y,x, carte):
-    if moveUp(y,x, carte):
-        background.setPosition(y-1,x)
+
+def up(y, x, carte):
+    if moveUp(y, x, carte):
+        background.setPosition(y - 1, x)
--- entrees.py
+++ entrees.py
@@ -1,36 +1,40 @@
-#coding: utf-8
+# coding: utf-8
 
 import sys, os, time, select, tty, termios, codecs, json
 
+ITERATIONS = 6
+objectif = 0
+objectifs_list = []
+repertoire = os.path.dirname(os.path.abspath(__file__))
 
-ITERATIONS=6
-objectif=0
-objectifs_list=[]
-repertoire=os.path.dirname(os.path.abspath(__file__))
 
 def set_objectif(i):
     global objectif
 
-    if(i<len(objectifs_list) and i==objectif+1):
-        objectif=int(i)
+    if (i < len(objectifs_list) and i == objectif + 1):
+        objectif = int(i)
+
 
 def set_objectifs_list(data):
     global objectifs_list
-    objectifs_list=data
+    objectifs_list = data
+
 
 def get_objectif():
     return objectif
 
+
 def get_text_objectif():
-    temp=get_objectif()
+    temp = get_objectif()
     return objectifs_list[temp]
 
+
 def chargement_fichier():
-    data_file=codecs.open(repertoire + '/objectives.json', 'r', encoding='utf-8')
+    data_file = codecs.open(repertoire + '/objectives.json', 'r', encoding='utf-8')
     data = json.loads(data_file.read())
     if ('objectif' in data):
-        set_objectif(data['objectif'])
+        set_objectif(int(data['objectif']))
 
-    if('objectifs' in data):
+    if ('objectifs' in data):
         set_objectifs_list(data['objectifs'])
     data_file.close()
